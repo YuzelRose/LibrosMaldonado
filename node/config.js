93f65cb227e4810f38bc033896e889fc44b94656
YuzelRose <zelinkmd@gmail.com>
2024-10-27 17:46:25 -0600

    v27_oct

--- node/config.js
+++ node/config.js
@@ -1,4 +1,7 @@
-export const  TRACT_ORIGIN = process.env.TRACT_ORIGIN || 'http://localhost:3000';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const  TRACT_ORIGIN = process.env.TRACT_ORIGIN || 'http://3.130.27.77';
 export const REQUEST_URL = process.env.REQUEST_URL || '/LibMal';
 export const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/LibMal';
 export const BACK_POT = process.env.BACK_POT || 5000
